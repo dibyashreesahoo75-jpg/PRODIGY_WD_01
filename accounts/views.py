035ad300b52e03daf479ce6563c46c582f8e8872
DIBYASHREE SAHOO <dibyashreesahoo75@gmail.com>
2026-07-07 13:06:18 +0530

    Completed Task 1 - User Authentication System

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
+
+
+@login_required
+def home(request):
+
+    if request.user.is_superuser:
+        return render(request, "accounts/admin_dashboard.html")
+
+    return render(request, "accounts/user_dashboard.html")
